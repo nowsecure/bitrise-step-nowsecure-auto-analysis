fa6a0e20bf8c53102e3f2e1a6939291dcad93be1
Spencer Koch <spencer.koch@reddit.com>
2020-01-17 12:20:46 -0600

    refactor for base functionality

--- step.sh
+++ step.sh
@@ -1,15 +1,11 @@
 #!/bin/bash
 set -ex
 
+# TODO: Update to handle curl errors (unable to upload / network issues)
+
 if [[ $nowsecure_group_id ]]
 then
-  output=$(curl -vv -H "Authorization: Bearer $nowsecure_api_token" -X POST https://lab-api.nowsecure.com/build/$nowsecure_group_id --data-binary @$upload_path)
+  output=$(curl -H "Authorization: Bearer $nowsecure_api_token" -X POST https://lab-api.nowsecure.com/build/$nowsecure_group_id --data-binary @$upload_path)
 else
-  output=$(curl -vv -H "Authorization: Bearer $nowsecure_api_token" -X POST https://lab-api.nowsecure.com/build/ --data-binary @$upload_path)
+  output=$(curl -H "Authorization: Bearer $nowsecure_api_token" -X POST https://lab-api.nowsecure.com/build/ --data-binary @$upload_path)
 fi
-
-echo $output | jq -je .ref
-if [ $? ]
-  exit 1
-fi
-exit 0
