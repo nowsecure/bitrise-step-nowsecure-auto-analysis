73b6253b37608c03b63549a7cd8cd3f934b6dfda
sp3nx0r <sp3nx0r@gmail.com>
2020-05-25 15:16:35 -0500

    Add groupid to url parameter for NowSecure API call (#2)

--- step.sh
+++ step.sh
@@ -5,7 +5,7 @@ set -ex
 
 if [[ $nowsecure_group_id ]]
 then
-  output=$(curl -H "Authorization: Bearer $nowsecure_api_token" -X POST https://lab-api.nowsecure.com/build/$nowsecure_group_id --data-binary @$upload_path)
+  output=$(curl -H "Authorization: Bearer $nowsecure_api_token" -X POST https://lab-api.nowsecure.com/build/?group=$nowsecure_group_id --data-binary @$upload_path)
 else
   output=$(curl -H "Authorization: Bearer $nowsecure_api_token" -X POST https://lab-api.nowsecure.com/build/ --data-binary @$upload_path)
 fi
