9e97312648f29ed8da11596128c233a54c246229
sp3nx0r <sp3nx0r@gmail.com>
2020-12-03 20:13:20 -0600

    add env var validation, add curl failure flag (#6)
    Co-authored-by: Spencer Koch <spencer.koch@reddit.com>

--- step.sh
+++ step.sh
@@ -1,11 +1,23 @@
 #!/bin/bash
 set -ex
 
-# TODO: Update to handle curl errors (unable to upload / network issues)
+# Required parameters
+if [ -z "${nowsecure_api_token}" ] ; then
+  echo " [!] Missing required input: nowsecure_api_token"
+  exit 1
+fi
+
+if [ -z "${upload_path}" ] ; then
+  echo " [!] Missing required input: upload_path"
+  exit 1
+fi
+
 
 if [[ $nowsecure_group_id ]]
 then
-  output=$(curl -H "Authorization: Bearer $nowsecure_api_token" -X POST https://lab-api.nowsecure.com/build/?group=$nowsecure_group_id --http1.1 --data-binary @$upload_path)
+  output=$(curl -H "Authorization: Bearer $nowsecure_api_token" -X POST https://lab-api.nowsecure.com/build/?group=$nowsecure_group_id --http1.1 --data-binary @$upload_path -f)
 else
-  output=$(curl -H "Authorization: Bearer $nowsecure_api_token" -X POST https://lab-api.nowsecure.com/build/ --http1.1 --data-binary @$upload_path)
+  output=$(curl -H "Authorization: Bearer $nowsecure_api_token" -X POST https://lab-api.nowsecure.com/build/ --http1.1 --data-binary @$upload_path -f)
 fi
+
+exit $?
